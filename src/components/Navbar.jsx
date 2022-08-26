f14d93f03e643c1e6387f41f73a667a1593b83b9
fedesiri <federicosiri9@gmail.com>
2022-08-26 19:56:35 -0300

    add icons github and linkedin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
-import { AppBar, Toolbar, Grid, Paper } from "@mui/material/";
+import { AppBar, Toolbar, Grid, Paper, IconButton } from "@mui/material/";
 import { grey } from "@mui/material/colors";
 import logo from "../assets/logo.webp";
-import logoWm from "../assets/logoWm.png";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const classes = {
     appbar: { backgroundColor: grey[800], paddingTop: "0.5%", paddingBottom: "0.5%" },
@@ -16,8 +17,25 @@ function Navbar() {
                         <Grid item>
                             <img src={logo} alt="" width="110px" />
                         </Grid>
+                    </Grid>
+                    <Grid
+                        container
+                        alignItems="center"
+                        justifyContent="flex-end"
+                        // style={{ backgroundColor: "white" }}
+                    >
                         <Grid item>
-                            <img src={logoWm} alt="" width="70px" />
+                            <IconButton
+                                href="https://www.linkedin.com/in/federico-siri/"
+                                target="none"
+                            >
+                                <LinkedInIcon sx={{ color: grey[100], fontSize: 35 }} />
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton href="https://github.com/fedesiri" target="none">
+                                <GitHubIcon sx={{ color: grey[100], fontSize: 35 }} />
+                            </IconButton>
                         </Grid>
                     </Grid>
                 </Toolbar>
